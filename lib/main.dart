62a1b93dd903bac30abfd84be8602738e5a77823
Kana Otawara <shimmer22.0204@gmail.com>
2021-07-07 23:58:01 +0900

    add sample widget

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -22,7 +23,26 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('タイトル'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Flutterで',
+                style: TextStyle(fontSize: 40),
+              ),
+              Text(
+                'アプリ開発',
+                style: TextStyle(fontSize: 40),
+              )
+            ]
+          )
+        )
+      )
     );
   }
 }
