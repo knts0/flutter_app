1aafa7746422e672dac71b8409a9fdcf69059aca
Kana Otawara <shimmer22.0204@gmail.com>
2021-07-12 00:18:18 +0900

    create next page

---
+++ lib/next_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class NextPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text('タイトルテスト'),
+      ),
+      body: Container(
+        color: Colors.red,
+      ),
+    );
+  }
+
+}
