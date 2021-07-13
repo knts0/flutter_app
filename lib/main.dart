13a9220910fa5a421c8655b8c61e359a8ace1c9e
Kana Otawara <shimmer22.0204@gmail.com>
2021-07-13 23:33:33 +0900

    pass data to next page

--- lib/main.dart
+++ lib/main.dart
@@ -82,7 +82,7 @@ class _MyHomePageState extends State<MyHomePage> {
           onPressed: () {
             Navigator.push(
               context,
-              MaterialPageRoute(builder: (context) => NextPage()),
+              MaterialPageRoute(builder: (context) => NextPage('value')),
             );
           },
         ),
--- lib/next_page.dart
+++ lib/next_page.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 
 class NextPage extends StatelessWidget {
+  NextPage(this.name);
+  String name;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text('タイトルテスト'),
+        title: Text(name),
       ),
       body: Container(
         color: Colors.red,
