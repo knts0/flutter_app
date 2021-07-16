d77e3e60e73d4cd49ec4dd7faa2ad7401aad2f7a
Kana Otawara <shimmer22.0204@gmail.com>
2021-07-16 23:58:10 +0900

    create form

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'タイトル')
+      home: MyCustomForm()
     );
   }
 }
@@ -88,6 +88,38 @@ class _MyHomePageState extends State<MyHomePage> {
             );
           },
         )
+      ),
+    );
+  }
+}
+
+// Define a custom Form widget.
+class MyCustomForm extends StatefulWidget {
+  @override
+  MyCustomFormState createState() {
+    return MyCustomFormState();
+  }
+}
+
+// Define a corresponding State class.
+// This class holds data related to the form.
+class MyCustomFormState extends State<MyCustomForm> {
+  // Create a global key that uniquely identifies the Form widget
+  // and allows validation of the form.
+  //
+  // Note: This is a `GlobalKey<FormState>`,
+  // not a GlobalKey<MyCustomFormState>.
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    // Build a Form widget using the _formKey created above.
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: <Widget>[
+          // Add TextFormFields and ElevatedButton here.
+        ],
       ),
     );
   }
